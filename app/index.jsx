060b00590c6fa1e47d8582aa3fe04bd5f41c5491
eshaan5 <eshaanbagga@gmail.com>
2024-08-05 23:37:36 +0530

    got the feel of react-native

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>My first mobile app :)</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">My first mobile app :)</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}>Profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
